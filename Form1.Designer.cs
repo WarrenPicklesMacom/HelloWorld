9004f546f45da00779ac9fa57ea7343d0d7d464a
WarrenPicklesMacom <warren.pickles@macom.com>
2016-02-23 11:54:43 +0000

    Version 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "(Version 2)";
+            this.label2.Text = "(Version 3)";
             // 
             // Form1
             // 
