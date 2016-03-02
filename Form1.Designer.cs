b0c743232193793b86acd2b21dfb391984b2a95b
WarrenPicklesMacom <warren.pickles@macom.com>
2016-03-02 10:38:24 +0000

    Trivial change to facilitate evaluation of Jenkins CI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,9 +53,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(48, 144);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "(Version 4)";
+            this.label2.Text = "Version 5";
             // 
             // buttonCancel
             // 
